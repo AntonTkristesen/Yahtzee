2da08a0713eb3c517a17b34b3ca89d600cc7ae60
dollas <mikkeldollaskontakt@gmail.com>
2024-02-20 09:14:52 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -109,7 +109,7 @@ namespace YahtzeeGame
 
         private void ScoreCategory()
             {
-                WriteLine("\nChoose a category to scre:");
+                WriteLine("\nChoose a category to score:");
                 foreach (var category in scorecard.Keys)
                 {
                     if (scorecard[category] == -1)
